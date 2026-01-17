bb9e3608859371f4096af54c6b57f41eb5dad21d
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-17 16:26:47 +0530

    add task and localstorage working

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function openClose(){
-    const allElems = document.querySelectorAll('.elem')
-const fullElem = document.querySelectorAll('.fullElem')
-const fullElemBackBtn = document.querySelectorAll('.fullElem .back')
+    let allElems = document.querySelectorAll('.elem')
+let fullElem = document.querySelectorAll('.fullElem')
+let fullElemBackBtn = document.querySelectorAll('.fullElem .back')
 
 allElems.forEach((elem)=>{
     elem.addEventListener('click', ()=>{
@@ -17,3 +17,54 @@ fullElemBackBtn.forEach((elem)=>{
 }
 
  openClose()
+
+let form = document.querySelector('.addTask form')
+let taskInput = document.querySelector('.addTask form input')
+let taskDetails = document.querySelector('.addTask form textarea')
+
+let taskCheckbox = document.querySelector('.addTask form .mark-imp #check')
+
+let currentTask = []
+
+if(localStorage.getItem('currentTask')){
+    currentTask = JSON.parse(localStorage.getItem('currentTask'))
+}
+else{
+    console.log('task list is empty');
+    
+}
+
+function renderTask(){
+     let allTask = document.querySelector('.allTask')
+ let sum = ''
+
+ currentTask.forEach((elem) => {
+    sum += `<div class="task">
+                <h5>${elem.task} <span class='${elem.imp}'> imp </span></h5>
+                <button>Mark as Completed</button>
+            </div>`
+ })
+
+ allTask.innerHTML = sum
+}
+
+renderTask()
+
+
+
+ form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    currentTask.push({
+        task: taskInput.value, 
+        details: taskDetails.value, 
+        imp: taskCheckbox.checked
+    })
+    taskInput.value = ''
+    taskDetails.value = ''
+    taskCheckbox.checked = ''
+
+    localStorage.setItem('currentTask', JSON.stringify(currentTask))
+
+    renderTask()
+ })
