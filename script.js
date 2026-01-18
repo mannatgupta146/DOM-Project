cc319457a9fe564f63b7819e4751656432af86f9
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-18 23:37:10 +0530

    daily palnner done

--- script.js
+++ script.js
@@ -92,8 +92,40 @@ allTask.addEventListener('click', (e) => {
   }
 })
 
+
+
+
+let dayPlanData = JSON.parse(localStorage.getItem('dayPlannerData')) || {}
+
+let dayPlanner = document.querySelector('.day-planner')
+
+let hours = Array.from({ length: 18 }, (elem, idx) => {
+  return `${6 + idx}:00 - ${7 + idx}:00`
+})
+
+let wholeDaySum = ''
+
+hours.forEach((elem, idx) => {
+  let savedData = dayPlanData[idx] || ''
+  wholeDaySum += `
+    <div class="day-planner-time">
+      <p>${elem}</p>
+      <input id="${idx}" type="text" placeholder="..." value="${savedData}">
+    </div>`
+})
+
+dayPlanner.innerHTML = wholeDaySum
+
+let dayPlannerInput = document.querySelectorAll('.day-planner input')
+
+dayPlannerInput.forEach((elem) => {
+  elem.addEventListener('input', () => {
+    dayPlanData[elem.id] = elem.value
+    localStorage.setItem('dayPlannerData', JSON.stringify(dayPlanData))
+  })
+})
+
+
+
+
 //next 
-
-
-
-
